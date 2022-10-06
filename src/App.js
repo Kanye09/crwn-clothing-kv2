fc64f2afe616be9bcbbb0bded424c55704599af1
Kanybek Almanbet <kanyalmanbet@gmail.com>
2022-10-05 22:10:26 -0500

    imported react router dom and implemented navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>lorem ipsum dolor sit amet</h1>
+    </div>
+  );
+};
 
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
